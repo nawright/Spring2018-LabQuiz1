542de0f036d79c2dc36913bde6bdbc85d8d5bb99
nawright <nawright@aggies.ncat.edu>
2019-02-25 15:09:44 -0500

    [Problem 4 is completed here]

--- CourseScheduler/src/coursescheduler/CourseScheduler.java
+++ CourseScheduler/src/coursescheduler/CourseScheduler.java
@@ -5,6 +5,11 @@
  */
 package coursescheduler;
 
+import java.io.File;
+import static java.lang.Integer.parseInt;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author Maste
@@ -31,6 +36,45 @@ public class CourseScheduler {
         //System.out.println(class1.toString());
         //System.out.println("");
         //System.out.println(class2.toString());
+        
+        ArrayList<Course> courses = new ArrayList<>();
+        readCourseData(courses,args[0]);
+    }
+    public static void readCourseData(ArrayList courses, String fileName) {
+        try {
+            Scanner input = new Scanner(new File(fileName));
+            while (input.hasNext()) {
+                String messy = input.nextLine();
+                String[] mess = new String[2];
+                mess = messy.split(",");
+                String name = mess[0].trim();
+                String semester = mess[1].trim();
+                String messy2 = input.nextLine();
+                String[] mess2 = messy2.split("\\*");
+                String firstName = mess2[0].trim();
+                String lastName = mess2[1].trim();
+                String building = mess2[2].trim();
+                String stringRoom = mess2[3].trim();
+                int room = parseInt(stringRoom);
+                String title = input.nextLine().trim();
+                String publish = input.nextLine().trim();
+                int edition = parseInt(input.nextLine());
+                Instructor instructor = new Instructor(firstName,lastName,building,room);
+                Textbook textbook = new Textbook (title,publish,edition);
+                Course course = new Course(name,semester,instructor,textbook);
+                courses.add(course);
+            }
+            int length = courses.size();
+            for ( int i = 0; i < length; i++) {
+                String print = courses.get(i).toString();
+                System.out.println(print);
+            }
+        }
+        catch(java.io.FileNotFoundException ex ) {
+            System.err.println("Could not find the file for printing");
+            System.exit(-1);
+        }
+        
     }
     
 }
